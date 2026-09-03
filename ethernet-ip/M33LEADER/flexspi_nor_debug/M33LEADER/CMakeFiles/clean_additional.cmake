10ab93ff379e070a920aa98d71a377d66268eb2f
pesaventoeil <pesavento@easterninstruments.com>
2026-09-03 15:30:11 -0400

    Initial compile attempt.

---
+++ ethernet-ip/M33LEADER/flexspi_nor_debug/M33LEADER/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,9 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "flexspi_nor_debug")
+  file(REMOVE_RECURSE
+  "clean_files-NOTFOUND"
+  "riop_EIP_M33LEADER_cm33.bin"
+  )
+endif()
---
+++ ethernet-ip/M33LEADER/flexspi_nor_debug/mcux_config.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _MCUX_CONFIG_H_
+#define _MCUX_CONFIG_H_
+
+#define SB_CONFIG_secondary_config debug
+#define SB_CONFIG_secondary_toolchain armgcc
+#define SB_CONFIG_secondary_core_id cm7
+
+#endif /* _MCUX_CONFIG_H_ */
